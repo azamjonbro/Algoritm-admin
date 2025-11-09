c522fe431ef80609b955184bf04072880d1ad270
azamjonbro <azamjondasturchi11@gmail.com>
2025-11-09 20:53:50 +0500

    added

--- src/Pages/Events/EvetModal.vue
+++ src/Pages/Events/EvetModal.vue
@@ -1,11 +1,44 @@
 <template>
   <transition name="slide-modal">
     <div class="modal">
-      
+      <div class="modal-content relative">
+         <Icons name="xIcon" class="xIcon" @click="closeModal" />
+        <h2>Sertifikat ma'lumotlarini qo‘shish</h2>
+        <form class="form" style="margin-top: 1rem;">
+          <div class="input-row">
+           
+          </div>
+
+          <div class="input-row">
+           
+          </div>
+
+          <div class="input-row">
+          
+          </div>
+
+          <div class="input-row">
+         
+          </div>
+
+          <button type="submit" class="submit-button">Yangilash</button>
+        </form>
+      </div>
     </div>
   </transition>
 </template>
 <script>
-export default {};
+import Icons from '@/components/Icons.vue';
+
+export default {
+  components:{
+    Icons
+  },
+  methods:{
+    closeModal(){
+
+    }
+  }
+};
 </script>
 <style lang=""></style>
