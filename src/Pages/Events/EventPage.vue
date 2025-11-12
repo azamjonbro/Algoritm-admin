403a792eb08aa77863463c46168e4843811efa61
azamjonbro <azamjondasturchi11@gmail.com>
2025-11-13 02:28:29 +0500

    added

--- src/Pages/Events/EventPage.vue
+++ src/Pages/Events/EventPage.vue
@@ -1,96 +1,93 @@
-
 <template>
- <div class="page">
-  <div class="page__top d-flex a-item j-between">
-   <h2>Events</h2>
-   <button class="create-button" @click="openModalFunc">Create events</button>
-  </div>
-  <div class="page-bottom ">
-   <div class="table scroll">
-    <div class="table-header">
-     <div class="row">
-      <div class="cell">Index</div>
-      <div class="cell">Title</div>
-      <div class="cell">Direction</div>
-      <div class="cell">Date</div>
-      <div class="cell d-flex j-end"></div>
-     </div>
+  <div class="page">
+    <div class="page__top d-flex a-item j-between">
+      <h2>Events</h2>
+      <button class="create-button" @click="openModalFunc">Create events</button>
     </div>
-    <div class="table-body">
-     <div class="row" v-for="(data, index) in events" :key="index">
-      <div class="cell">{{ index+1 }}</div>
-      <div class="cell">{{ data.title }}</div>
-      <div class="cell">{{ data.direction }}</div>
-      <div class="cell">{{ formatDate(data.createdAt) }}</div>
-      <div class="cell d-flex gap12 j-end">
-       <Icons name="edit" class="icon info" @click="editEvent(data)" />
-       <Icons name="deleted" class="icon danger" @click="deleteEvent(data.id)" />
+    <div class="page-bottom">
+      <div class="table scroll">
+        <div class="table-header">
+          <div class="row">
+            <div class="cell">Index</div>
+            <div class="cell">Title</div>
+            <div class="cell">Direction</div>
+            <div class="cell">Date</div>
+            <div class="cell d-flex j-end"></div>
+          </div>
+        </div>
+        <div class="table-body">
+          <div class="row" v-for="(data, index) in events" :key="index">
+            <div class="cell">{{ index + 1 }}</div>
+            <div class="cell">{{ data.title }}</div>
+            <div class="cell">{{ data.direction }}</div>
+            <div class="cell">{{ formatDate(data.createdAt) }}</div>
+            <div class="cell d-flex gap12 j-end">
+              <!-- <Icons name="edit" class="icon info" @click="editEvent(data)" /> -->
+              <Icons name="deleted" class="icon danger" @click="deleteEvent(data._id)" />
+            </div>
+          </div>
+        </div>
       </div>
-     </div>
     </div>
-   </div>
   </div>
- </div>
- <EventModel v-if="openModal" @close="closeModal($event)"/>
+  <EventModel v-if="openModal" @close="closeModal($event)" />
 </template>
 <script>
 import Icons from "@/components/Icons.vue";
-import api from "@/Utils/axios"
-import EventModel from "./EvetModal.vue"
+import api from "@/Utils/axios";
+import EventModel from "./EvetModal.vue";
 export default {
   name: "EventPage",
   components: {
     Icons,
-    EventModel
+    EventModel,
   },
   data() {
     return {
       events: [],
-      openModal:false,
+      openModal: false,
       searchParameter: "",
     };
   },
-  computed: {
-    
-  },
+  computed: {},
   methods: {
-    async getAllEvents(){
+    async getAllEvents() {
       try {
         const response = await api.get("/api/videos/video-opinions");
-        
+
         this.events = response.data;
         console.log(response.data);
-        
       } catch (error) {
         console.error("Error fetching events:", error);
       }
     },
     formatDate(dateString) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      const options = { year: "numeric", month: "long", day: "numeric" };
       return new Date(dateString).toLocaleDateString(undefined, options);
     },
     openModalFunc() {
-      this.openModal=true
-      // Logic to open modal for creating new event
+      this.openModal = true;
     },
-    closeModal(close){
-      console.log(close);
-      
-      this.openModal=false
+    closeModal() {
+      this.openModal = false;
+      this.getAllEvents();
     },
-    editEvent() {
-      // Logic to edit the selected event
+    deleteEvent(id) {
+      api
+        .delete(`/api/videos/video-opinions/${id}`)
+        .then(() => {
+          this.events = this.events.filter((event) => event.id !== id);
+          this.getAllEvents();
+        })
+        .catch((error) => {
+          console.error("Error deleting event:", error);
+        });
     },
-    deleteEvent() {
-      // Logic to delete the selected event
-    }
   },
   mounted() {
-    this.getAllEvents() 
-  }
-}
+    this.getAllEvents();
+  },
+};
 </script>
 
-<style >
- 
-</style>
+<style></style>
